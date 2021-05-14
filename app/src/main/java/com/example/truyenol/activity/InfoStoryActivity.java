36078e1af2b6c95e70ce3116b27ba4821711b98d
BlueFrankie <apham8351@gmail.com>
2021-05-14 19:51:45 +0700

    new

--- app/src/main/java/com/example/truyenol/activity/InfoStoryActivity.java
+++ app/src/main/java/com/example/truyenol/activity/InfoStoryActivity.java
@@ -1,14 +1,16 @@
-package com.example.truyenol;
+package com.example.truyenol.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.truyenol.R;
+
+public class InfoStoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_infostory);
     }
 }
