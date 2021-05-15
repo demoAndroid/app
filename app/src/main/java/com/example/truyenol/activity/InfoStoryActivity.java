23309b69e4b3413daea5a8df853c6edeb927e46a
BlueFrankie <apham8351@gmail.com>
2021-05-15 11:59:07 +0700

    activity

--- app/src/main/java/com/example/truyenol/activity/InfoStoryActivity.java
+++ app/src/main/java/com/example/truyenol/activity/InfoStoryActivity.java
@@ -2,7 +2,9 @@ package com.example.truyenol.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.truyenol.R;
 
@@ -12,5 +14,11 @@ public class InfoStoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infostory);
+
     }
+    public void showChapter(View view){
+        Intent intent1 = new Intent(this, StoryChapterActivity.class);
+        startActivity(intent1);
+    }
+
 }
