0e47a4de1f4edf53291ef853001865d2c73d6abc
daodat2801 <daothanhdat2801@gmail.com>
2021-05-16 17:18:02 +0700

    Update file

---
+++ app/src/main/java/com/example/truyenol/activity/ModifyStory/ModifyChapter.java
@@ -0,0 +1,16 @@
+package com.example.truyenol.activity.ModifyStory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.truyenol.R;
+
+public class ModifyChapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_chapter);
+    }
+}
