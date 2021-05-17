e45016bb1bb8dce0f048bd2a4991a07575fe7a49
daodat2801 <daothanhdat2801@gmail.com>
2021-05-18 00:00:15 +0700

    Update file

--- app/src/main/java/com/example/truyenol/activity/ModifyStory/ModifyChapter.java
+++ app/src/main/java/com/example/truyenol/activity/ModifyStory/ModifyChapter.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.truyenol.R;
 import com.example.truyenol.model.Chapter;
@@ -18,38 +19,27 @@ import java.util.ArrayList;
 
 public class ModifyChapter extends AppCompatActivity {
     private Spinner chapterSpn;
-    private EditText contentTxt,nameChapTxt,chapNumberTxt;
-    private Button saveBtn,confBtn,conf1Btn;
+    private EditText contentTxt, nameChapTxt, chapNumberTxt;
+    private Button saveBtn, confBtn, conf1Btn;
     private ArrayList<Chapter> chapterList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_chapter);
         //Gán biến view
-        nameChapTxt=findViewById(R.id.nameChapTxt);
-        chapNumberTxt=findViewById(R.id.chapNumberTxt);
-        chapterSpn=findViewById(R.id.chapterSpn);
-        contentTxt=findViewById(R.id.contentTxt);
-        saveBtn=findViewById(R.id.saveBtn);
-        conf1Btn=findViewById(R.id.conf1Btn);
-        confBtn=findViewById(R.id.confBtn);
+        nameChapTxt = findViewById(R.id.nameChapTxt);
+        chapNumberTxt = findViewById(R.id.chapNumberTxt);
+        chapterSpn = findViewById(R.id.chapterSpn);
+        contentTxt = findViewById(R.id.contentTxt);
+        saveBtn = findViewById(R.id.saveBtn);
+        conf1Btn = findViewById(R.id.conf1Btn);
+        confBtn = findViewById(R.id.confBtn);
         contentTxt.setEnabled(false);
         nameChapTxt.setEnabled(false);
         chapterSpn.setEnabled(false);
         saveBtn.setEnabled(false);
         confBtn.setEnabled(false);
-        //Set chapterList
-        Intent intent=ModifyChapter.this.getIntent();
-        int n=Integer.parseInt(intent.getStringExtra("chapNumber"));
-        String[] oneToN=new String[n];
-        chapterList=new ArrayList<Chapter>();
-        for(int i=0;i<n;i++){
-            chapterList.add(new Chapter("",""));
-            oneToN[i]=""+(i+1);
-        }
-        ArrayAdapter adapter=new ArrayAdapter(this, android.R.layout.simple_spinner_item, oneToN);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        chapterSpn.setAdapter(adapter);
         conf1Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,7 +49,8 @@ public class ModifyChapter extends AppCompatActivity {
         confBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setChapters(chapterSpn.getSelectedItem().toString());
+                int index=Integer.parseInt(chapterSpn.getSelectedItem().toString())-1;
+                setChapters(index);
 
             }
         });
@@ -70,41 +61,62 @@ public class ModifyChapter extends AppCompatActivity {
             }
         });
     }
-    public void setChapters(String chapterNumber){
-        if(confBtn.getText().toString()=="Chọn"){
-           chapterSpn.setEnabled(false);
-           saveBtn.setEnabled(false);
-           confBtn.setText("OK");
-        }
-        else{
-            confBtn.setText("Chọn");
-            chapterList.set(Integer.parseInt(chapterNumber)-1,new Chapter(nameChapTxt.getText().toString(),contentTxt.getText().toString()));
-        }
-    }
-    public void doSaveResult(){
-        Intent intent=new Intent();
-         for(Integer i=0;i<2*chapterList.size();i+=2){
-            intent.putExtra(i.toString(),chapterList.get(i).getNameChapter());i++;
-             intent.putExtra(i.toString(),chapterList.get(i).getNameChapter());
-         }
-         setResult(RESULT_OK,intent);
-         finish();
-    }
-    public void confirmChapterNumber(){
-        if(conf1Btn.getText().toString()=="OK"){
-            conf1Btn.setText("Hủy");
+
+    public void setChapters(int index) {
+        if (chapterSpn.isEnabled() == true) {
+            chapterSpn.setEnabled(false);
             contentTxt.setEnabled(true);
+            contentTxt.setText(chapterList.get(index).getContent());
+            nameChapTxt.setText(chapterList.get(index).getNameChapter());
             nameChapTxt.setEnabled(true);
+            confBtn.setText("OK");
+
+        } else {
+            confBtn.setText("Chọn");
+            nameChapTxt.setEnabled(false);
+            contentTxt.setEnabled(false);
+            chapterSpn.setEnabled(true);
+            chapterList.set(index, new Chapter(nameChapTxt.getText().toString(), contentTxt.getText().toString()));
+        }
+    }
+
+    public void doSaveResult() {
+        Intent intent = new Intent();
+        for (Integer i = 0,z=0; i < chapterList.size(); i++) {
+            intent.putExtra(z.toString(), chapterList.get(i).getNameChapter());z++;
+            intent.putExtra(z.toString(), chapterList.get(i).getContent());z++;
+        }
+        intent.putExtra("chapterNumber", String.valueOf(chapterList.size()));
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    public void confirmChapterNumber() {
+        if (confBtn.isEnabled() == false) {
+            conf1Btn.setText("Hủy");
             chapterSpn.setEnabled(true);
             saveBtn.setEnabled(true);
             confBtn.setEnabled(true);
-        }else{
+            chapNumberTxt.setEnabled(false);
+            //Set chapterList
+            int n = Integer.parseInt(chapNumberTxt.getText().toString());
+            String[] oneToN = new String[n];
+            chapterList = new ArrayList<Chapter>();
+            for (int i = 0; i < n; i++) {
+                chapterList.add(new Chapter("", ""));
+                oneToN[i] = "" + (i + 1);
+            }
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, oneToN);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            chapterSpn.setAdapter(adapter);
+        } else {
             conf1Btn.setText("OK");
             contentTxt.setEnabled(false);
+            saveBtn.setEnabled(false);
             nameChapTxt.setEnabled(false);
             chapterSpn.setEnabled(false);
-            saveBtn.setEnabled(false);
             confBtn.setEnabled(false);
+            chapNumberTxt.setEnabled(true);
         }
     }
 }
